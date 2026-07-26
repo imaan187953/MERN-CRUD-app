3eeeed6af4a51d109ce6c65cdcee2f542b7d0985
Eman Sajjad <akzachoudhry1123@gmail.com>
2026-07-26 22:18:48 +0500

    first commit

---
+++ frontend/src/App.jsx
@@ -0,0 +1,14 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+    <h6>MERN APP</h6>
+    </>
+  )
+}
+
+export default App
