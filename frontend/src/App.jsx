5d3d05cd7d1678905f40b8d64ed691bc85341ac1
Eman Sajjad <akzachoudhry1123@gmail.com>
2026-07-27 14:48:12 +0500

    add components

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,13 +1,24 @@
 import { useState } from 'react'
 import './App.css'
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import 'bootstrap/dist/css/bootstrap.min.css';
+import User from './components/User';
+import CreateUser from './components/CreateUser';
+import UpdateUser from './components/UpdateUser';
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
-    <>
-    <h6>MERN APP</h6>
-    </>
+   <div>
+    <BrowserRouter>
+    <Routes>
+      <Route path='/' element={<User />}></Route>
+      <Route path='/create' element={<CreateUser />}></Route>
+      <Route path='/update' element={<UpdateUser />}></Route>
+    </Routes>
+    </BrowserRouter>
+   </div>
   )
 }
 
---
+++ frontend/src/components/User.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const User = () => {
+  return (
+    <div className='d-flex vh-100 bg-primary justify-content-center align-items-center'>
+      <div className='w-50 bg-white rounded p-3'>
+           oho
+      </div>
+    </div>
+  )
+}
+
+export default User
